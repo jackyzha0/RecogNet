e47cf57cb3d7a1dd132b5d88e88d0ab3847d4047
Jacky <jacky.zhao2019@stgeorges.bc.ca>
2019-04-10 12:05:52 -0700

    Working AWS

--- get_image.py
+++ get_image.py
@@ -5,18 +5,22 @@ import boto3
 ENDPOINT = "https://rekognition.us-west-2.amazonaws.com/detectlabels"
 file_loc = "imgs/tmp.png"
 
+minconf = 75
 
 def initCam():
-    cthrow('Loaded OpenCV', type='INFO')
-
+    cthrow('Loaded OpenCV', type='OK')
     cap = cv2.VideoCapture(0)
-    return cap
 
     # Check if the webcam is opened correctly
     if not cap.isOpened():
+        cthrow('Camera cannot be initialized', type="ERR")
         raise IOError("Cannot open webcam")
+    else:
+        cthrow('Camera', type='OK')
+    return cap
 
 def getCap(cap):
+    cthrow('Capture Start', type='INFO')
     ret, frame = cap.read()
     print(frame.shape)
     cap.release()
@@ -24,16 +28,19 @@ def getCap(cap):
 
 cap = initCam()
 frame = getCap(cap)
+cthrow('Frame retrieved', type='OK')
 cv2.imwrite(file_loc, frame)
+cthrow('Write file', type='OK')
 
 # BEGIN AWS SERVICES
 client=boto3.client('rekognition')
 
-with open(imageFile, 'rb') as image:
-    response = client.detect_labels(Image={'Bytes': image.read()})
+with open(file_loc, 'rb') as image:
+    response = client.detect_labels(Image={'Bytes': image.read()}, MinConfidence=minconf)
 
-print('Detected labels in ' + file_loc)
 for label in response['Labels']:
-    print (label['Name'] + ' : ' + str(label['Confidence']))
+    if label['Name'] == 'Person':
+        print(label['Instances'])
+        print(label['Name'])
 
 print('Done...')
